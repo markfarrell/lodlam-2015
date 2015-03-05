7d174a558375b1af106d5d92a4cf2673b93d6b46
Mark Farrell <m4farrel@uwaterloo.ca>
2015-03-05 16:16:04 -0400

    Set material; scale terrain; crop raster images that are too large.
    Signed-off-by: Mark Farrell <m4farrel@uwaterloo.ca>

--- Assets/Arcadia/Scripts/elevation.clj
+++ Assets/Arcadia/Scripts/elevation.clj
@@ -20,62 +20,85 @@
                       max-lat))
      texture))
 
-(defn texture->grayscales
-  "Takes a 2d texture; produces a list of grayscale values of its colors (floats)."
-  [texture]
-  (map (fn [[x z]]
-           (.grayscale (.GetPixel texture x z)))
-       (muninn/grid (.width texture)
-                    (.height texture))))
-
 (defn texture->vertices
   "Takes a 2d texture; produces a list of vertices."
-  [texture]
+  [texture width length]
+  (let [y-scale (* 145.0 10.0)
+        x-scale 90
+        z-scale 90]
+    (map (fn [[x z]]
+             (Vector3. (* x x-scale)
+                       (* (.grayscale (.GetPixel texture x z)) y-scale)
+                       (* z z-scale)))
+         (muninn/grid width
+                      length))))
+
+(defn plane-triangle-indices
+  [width length]
+  (mapcat
+    (fn [[x z]]
+      (list (+ (* z width) x)
+            (+ (* (inc z) width) x)
+            (+ (* z width)
+               (inc x))
+            (+ (* (inc z) width) x)
+            (+ (* (inc z) width)
+               (inc x))
+            (+ (* z width)
+               (inc x))))
+    (grid (dec width)
+          (dec length))))
+
+(defn plane-vertices
+  [width length height-at]
   (map (fn [[x z]]
            (Vector3. x
-                     (* (.grayscale (.GetPixel texture x z)) 145)
+                     (height-at x z)
                      z))
-       (muninn/grid (.width texture)
-                    (.height texture))))
+       (grid width length)))
 
-(defn texture->uvs
-  "Takes a 2d texture; produces a list of normals."
-  [texture]
-  (map (fn [[x z]]
-         (Vector2. (* x
-                      (/ 1
-                         (max (dec (.width texture)) 1)))
-                   (*  z
-                      (/ 1
-                         (max (dec (.height texture)) 1)))))
-       (muninn/grid (.width texture)
-                    (.height texture))))
+(defn plane-uvs
+  [width length]
+    (map (fn [[x z]]
+           (Vector2. (* x
+                        (/ 1
+                           (max (dec width) 1)))
+                     (*  z
+                        (/ 1
+                           (max (dec length) 1)))))
+         (grid width length)))
 
-(defn texture->triangle-indices
-  "Takes a 2d texture; produces a list of triangle indices."
-  [texture]
-  (muninn/plane-triangle-indices (.width texture)
-                                 (.height texture)))
-
-(defn texture->plane
-  "Takes a 2d texture; produces a plane mesh."
-  [texture]
+(defn create-terrain
+  "Takes collections of vertices, uvs and triangle indices; produces a game object."
+  [vertices uvs triangle-indices]
   (let [mesh (Mesh.)
         plane (GameObject.)]
     (do (set! (.vertices mesh)
-              (into-array (texture->vertices texture)))
+              (into-array vertices))
         (set! (.uv mesh)
-              (into-array (texture->uvs texture)))
+              (into-array uvs))
         (set! (.triangles mesh)
-              (int-array (texture->triangle-indices texture)))
+              (int-array triangle-indices))
         (.RecalculateNormals mesh)
         (.RecalculateBounds mesh)
         (set! (.mesh (.AddComponent plane "MeshFilter")) mesh)
-        (.AddComponent plane "MeshRenderer")
+        (set! (.material (.AddComponent plane "MeshRenderer"))
+              (UnityEngine.Resources/Load "Terrain" UnityEngine.Material))
+        (.AddComponent plane "MeshCollider")
         plane)))
+
+(defn texture->terrain
+  "Takes a 2d texture; produces a game object."
+  [texture]
+  (let [width (min (.width texture) 254)
+        length (min (.height texture) 254)
+        vertices (texture->vertices texture width length)
+        uvs (muninn/plane-uvs width length)
+        triangle-indices (muninn/plane-triangle-indices width length)]
+    (create-terrain vertices uvs triangle-indices)))
 
 (defcomponent Elevation [^UnityEngine.Texture2D height-map ^float min-long ^float min-lat ^float max-long ^float max-lat]
   (Start [this]
          (do (set! (. this height-map)
                    (get-height-map min-long min-lat max-long max-lat))
-             (texture->plane (. this height-map)))))
+             (texture->terrain (. this height-map)))))
--- Assets/Arcadia/Scripts/muninn.clj
+++ Assets/Arcadia/Scripts/muninn.clj
@@ -22,55 +22,6 @@
                  (range width)))
           (range length)))
 
-(defn plane-triangle-indices
-  [width length]
-  (mapcat
-    (fn [[x z]]
-      (list (+ (* z width) x)
-            (+ (* (inc z) width) x)
-            (+ (* z width)
-               (inc x))
-            (+ (* (inc z) width) x)
-            (+ (* (inc z) width)
-               (inc x))
-            (+ (* z width)
-               (inc x))))
-    (grid (dec width)
-          (dec length))))
-
-(defn plane-vertices
-  [width length]
-  (map (fn [[x z]]
-           (Vector3. x 0.0 z))
-       (grid width length)))
-
-(defn plane-uvs
-  [width length]
-    (map (fn [[x z]]
-           (Vector2. (* x
-                        (/ 1
-                           (max (dec width) 1)))
-                     (*  z
-                        (/ 1
-                           (max (dec length) 1)))))
-         (grid width length)))
-
-(defn create-plane
-  [width length]
-  (let [mesh  (Mesh.)
-        plane (GameObject.)]
-    (do (set! (.vertices mesh)
-              (into-array (plane-vertices width length)))
-        (set! (.uv mesh)
-              (into-array (plane-uvs width length)))
-        (set! (.triangles mesh)
-              (int-array (plane-triangle-indices width length)))
-        (.RecalculateNormals mesh)
-        (.RecalculateBounds mesh)
-        (set! (.mesh (.AddComponent plane "MeshFilter")) mesh)
-        (.AddComponent plane "MeshRenderer")
-        plane)))
-
 (defn set-main-texture!
       "Expects a GameObject; sets main texture."
       [game-object texture]
