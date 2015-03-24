dd3f1f235896d7e19a9b82c5ef8e57d3b9968343
Mark Farrell <m4farrel@uwaterloo.ca>
2015-03-24 14:37:01 -0300

    Texture subtract.
    Signed-off-by: Mark Farrell <m4farrel@uwaterloo.ca>

--- Assets/Arcadia/Scripts/elevation.clj
+++ Assets/Arcadia/Scripts/elevation.clj
@@ -9,9 +9,9 @@
 (def partition-width 254)
 (def partition-length 254)
 
-(def x-scale 90.0)
-(def y-scale (* 145.0 20))
-(def z-scale 90.0)
+(def x-scale 2.0)
+(def y-scale 255.0)
+(def z-scale 2.0)
 
 (defn get-height-map
   "Takes values for min/max longitude and latitudes. Produces a 2d texture of height data."
@@ -92,23 +92,71 @@
         (set! (.name plane) "Terrain")
         plane)))
 
-(defn texture->terrain
-  "Takes a 2d texture; produces a game object."
-  [texture]
-  (let [width (min (.width texture) partition-width)
-        length (min (.height texture) partition-length)
-        vertices (texture->vertices texture width length)
+(defn vertices->terrain
+  "Takes a list of vertices, a width and length; produces a game object."
+  [vertices vertices-width vertices-length]
+  (let [width (min vertices-width partition-width)
+        length (min vertices-length partition-length)
         uvs (plane-uvs width length)
         triangle-indices (plane-triangle-indices width length)]
     (create-terrain vertices uvs triangle-indices)))
 
-(defn create-terrains
-  [min-long min-lat max-long max-lat]
-  (let [height-map (get-height-map min-long min-lat max-long max-lat)
-        texture-partitions (muninn/texture-partitions height-map partition-width partition-length)
+(defn texture->terrain
+  "Takes a 2d texture; produces a game object."
+  [texture]
+  (let [width (.width texture)
+        length (.height texture)]
+  (vertices->terrain
+    (texture->vertices texture width length) width length)))
+
+(defn color-clamp
+  [x]
+  (min 1.0
+       (max x 0.0)))
+
+(defn color-subtract
+  [color-2 color-1]
+  (UnityEngine.Color.
+    (color-clamp
+      (- (.r color-2)
+         (* (.a color-1)
+            (.r color-1))))
+    (color-clamp
+      (- (.g color-2)
+         (* (.a color-1)
+            (.g color-1))))
+    (color-clamp
+      (- (.b color-2)
+         (* (.a color-1)
+            (.b color-1))))
+    (.a color-2)))
+
+(defn colors-subtract
+  [texture-2 texture-1]
+  (let [width (.width texture-2)
+        length (.height texture-2)]
+    (map (fn [[x z]]
+           (color-subtract
+             (.GetPixel texture-2 x z)
+             (.GetPixel texture-1 x z)))
+         (muninn/grid width length))))
+
+(defn texture-subtract
+  [texture-2 texture-1]
+  (let [width (.width texture-2)
+        length (.height texture-2)
+        new-texture (Texture2D. width length)]
+    (do (.SetPixels new-texture
+                    (into-array (colors-subtract texture-2 texture-1)))
+        (.Apply new-texture)
+        new-texture)))
+
+(defn texture->terrains
+  [texture]
+  (let [texture-partitions (muninn/texture-partitions texture partition-width partition-length)
         terrain-objects (map texture->terrain texture-partitions)
-        terrain-positions (muninn/grid-partitions-positions (.width height-map)
-                                                            (.height height-map)
+        terrain-positions (muninn/grid-partitions-positions (.width texture)
+                                                            (.height texture)
                                                             partition-width
                                                             partition-length)]
     (map (fn [[terrain-object [x z]]]
@@ -118,6 +166,10 @@
                            (* z z-scale))
                terrain-object))
          (map vector terrain-objects terrain-positions))))
+
+(defn create-terrains
+  [min-long min-lat max-long max-lat]
+  (texture->terrains (get-height-map min-long min-lat max-long max-lat)))
 
 (defcomponent Elevation [^float min-long ^float min-lat ^float max-long ^float max-lat]
   (Start [this]
